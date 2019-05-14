cbe761d26dd213f0ffc7c3fe7cf413020523d2be
Joe Jamsky <cba1067950@yahoo.com>
2019-05-14 20:06:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog 
   def name=(dog_name)
-    this_dog_name = dog_name
+    this_dogs_name = dog_name
   end
   
   def name 
-    this_dog_name
+    this_dogs_name
   end
 end
 
 lassie = Dog.new
-lassie.name("Lassie")
+lassie.name = "Lassie"
 Lassie.name
